a1392c8ae529d7fb4a776808de5997fc5a6a1b09
Moudi <mjaderi97@gmail.com>
2023-10-19 15:50:53 +1100

    Added weather api to project

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,29 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+const apiKey = "595864bfef3aa4eab4181ee9b9f3e020"
+
+// fetch data from server
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}5&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeoCode(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=`
+    }
+}
